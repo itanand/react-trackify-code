bc3547b16159cdabb0b8ae68690b2be3d5b82be3
Anand Mohan <anand1505mohan@gmail.com>
2022-12-21 11:12:39 +0530

    trackify-API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Menu from './components/Menu';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import MyProfile from './components/MyProfile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+          <Route path='/myprofile' element={<MyProfile />}></Route>
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </>
   );
 }
 
